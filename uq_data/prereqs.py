5d4fd537677d98ca10020c3721915e9d842c7cd9
Kenton Lam <kenton_lam@outlook.com>
2019-05-02 11:24:04 +1000

    more peg.

--- uq_data/prereqs.py
+++ uq_data/prereqs.py
@@ -1,6 +1,9 @@
 from dataclasses import dataclass, field
 from typing import List
 
+from parsimonious.grammar import Grammar
+from parsimonious.exceptions import ParseError
+
 static_field = lambda name: field(default=name, init=False, repr=False, compare=False)
 
 @dataclass
@@ -49,3 +52,57 @@ def _pretty_list(node, depth=0):
         return s
     else:
         return (indent+str(node), )
+
+# https://math.stackexchange.com/questions/140036/why-is-this-parsing-expression-grammar-left-recursive
+_grammar = Grammar('''
+    prereq = expr eof
+
+    expr = (parens!(and/or)) / basic_comma_list / basic_and_list / basic_or_list
+    parens = '(' expr ')'
+    
+    basic_comma_list = course!(and/or) more_commas
+    more_commas = (',' space course more_commas) / ''
+    comma = "," / ";"
+
+    basic_and_list = course!or more_ands
+    more_ands = (and course more_ands) / ''
+    and = space ("and" / "&" / "+") space
+
+    basic_or_list = course more_ors
+    more_ors = (or course more_ors) / '' 
+    or = space ("or" / "|") space
+
+    course = code / parens
+    code = ~r"[A-Z]{3,5}[0-9]{3,4}[A-Z]?"
+
+    space = ~r"\\s+"
+
+    eof = !~"."
+
+''')
+def parse_prereq(prereq_string: str):     
+    return _grammar.parse(prereq_string)
+
+if __name__ == "__main__":
+    with open('_prereqs.txt') as f:
+        failures = 0
+        total = 0
+        for l in f:
+            total += 1
+            l = l.strip()
+            try:
+                result = parse_prereq(l)
+            except ParseError as e:
+                failures += 1
+                print('## failed', l)
+                print(e)
+            else:
+                # print(failures, 'failed.')
+                print(result)
+            print()
+            if total == 2:
+                pass
+                # break
+
+        print()
+        print('failed:', failures, '/', total)
