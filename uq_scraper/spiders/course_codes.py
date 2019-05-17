1014ca2c65a3e5eec0b6882b54856b910e52a18d
Kenton Lam <kenton_lam@outlook.com>
2019-05-17 21:49:52 +1000

    Scraping course codes with scrapy.

---
+++ uq_scraper/spiders/course_codes.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class CourseCodesSpider(scrapy.Spider):
+    name = 'course_codes'
+    allowed_domains = ['my.uq.edu.au']
+    start_urls = ['https://my.uq.edu.au/programs-courses/search.html?keywords=course&searchType=all&archived=false/']
+
+    def parse(self, response):
+        for li in response.css('#courses-container .listing > li'):
+            yield {
+                'code': li.css('.code::text').get(),
+                'name': li.css('.title::text').get(),
+                'href': li.css('a[href^="/programs-courses/"]::attr(href)').get()
+            }
