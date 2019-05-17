3553b9b02b6aa87c518d9b4bd3fe28e8c633b82d
Kenton Lam <kenton_lam@outlook.com>
2019-05-17 23:18:59 +1000

    Check for already scraped courses, added fields.
    Added current, description and _updated.

--- uq_scraper/spiders/course_details.py
+++ uq_scraper/spiders/course_details.py
@@ -1,11 +1,23 @@
 # -*- coding: utf-8 -*-
 import scrapy
 import json 
+import datetime
 
 BASE = 'https://my.uq.edu.au'
 
+seen = set()
+with open('./data/course_details.json') as f:
+    for l in f:
+        if l.startswith('{"code": "'):
+            l = l[:30]
+            l = l.replace('{"code": "', '', 1)
+            code, _ = l.split('"', 1)
+            seen.add(code)
+print(len(seen), 'items already done.')
+print()
+
 with open('./data/courses.json') as f:
-    course_urls = [BASE+x['href'] for x in json.load(f)]
+    course_urls = [BASE+x['href'] for x in json.load(f) if x['code'] not in seen]
 
 class CourseDetailsSpider(scrapy.Spider):
     name = 'course_details'
@@ -14,7 +26,8 @@ class CourseDetailsSpider(scrapy.Spider):
 
     custom_settings = {
         'CONCURRENT_REQUESTS': 3,
-        'CONCURRENT_REQUESTS_PER_DOMAIN': 3
+        'CONCURRENT_REQUESTS_PER_DOMAIN': 3,
+        'RETRY_TIMES': 10,
     }
 
     _fields = {
@@ -36,14 +49,22 @@ class CourseDetailsSpider(scrapy.Spider):
     def parse(self, response):
         out = {
             'code': response.css('#course-title::text').get().split('(')[-1][:-1],
-            'name': response.css('.breadcrumb-wrapper>ul>li:nth-child(2)::text').get()
+            'name': response.css('.breadcrumb-wrapper>ul>li:nth-child(2)::text').get(),
+            'current': response.css('#course-current-offerings').get() is not None,
+            '_updated': datetime.datetime.utcnow().isoformat()
         }
+
+        desc = '\n'.join(response.css('#course-summary ::text').getall()).strip()
+        out['description'] = desc
+
         left = response.css('#summary-content')
         for key, name in self._fields.items():
             value = left.css(f'#{key} ::text').getall() or None
             if value:
                 value = ' '.join(value).strip()
             out[name] = value
+
+            
         
         out['offerings'] = offers = []
         for tr in response.css('.offerings > tbody > tr'):
